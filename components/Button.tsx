882a76ff0bf37b665ec360cd80d0130e1e439b46
Pritam Dhara <46365255+pritam1813@users.noreply.github.com>
2024-12-22 17:35:36 +0530

    commiting failed test

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = () => {
   return (
     <button className="text-xl px-3 py-1 bg-slate-500 text-white rounded-full hover:bg-yellow-500 hover:text-stone-900 border-none">
-      Submit
+      Genius
     </button>
   );
 };
