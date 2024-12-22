5131b005116f4698a5c085615392c0c8394e916d
Pritam Dhara <46365255+pritam1813@users.noreply.github.com>
2024-12-22 17:47:05 +0530

    corrected for passing test

--- __tests__/Button.test.tsx
+++ __tests__/Button.test.tsx
@@ -4,13 +4,13 @@ import Button from "@/components/Button";
 describe("Button component", () => {
   test("renders the button with correct text", () => {
     render(<Button />);
-    const buttonElement = screen.getByRole("button", { name: /submit/i });
+    const buttonElement = screen.getByRole("button", { name: /Genius/i });
     expect(buttonElement).toBeInTheDocument();
   });
 
   test("has the correct initial class names", () => {
     render(<Button />);
-    const buttonElement = screen.getByRole("button", { name: /submit/i });
+    const buttonElement = screen.getByRole("button", { name: /Genius/i });
     expect(buttonElement).toHaveClass(
       "text-xl px-3 py-1 bg-slate-500 text-white rounded-full hover:bg-yellow-500 hover:text-stone-900 border-none"
     );
@@ -18,7 +18,7 @@ describe("Button component", () => {
 
   test("changes class names on hover", () => {
     render(<Button />);
-    const buttonElement = screen.getByRole("button", { name: /submit/i });
+    const buttonElement = screen.getByRole("button", { name: /Genius/i });
     expect(buttonElement).toHaveClass(
       "hover:bg-yellow-500 hover:text-stone-900"
     );
